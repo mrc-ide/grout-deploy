07ec947bc5b431a6691dbb6c94a7fb8b39edb712
Emma <EmmaRussellKilburn@gmail.com>
2025-01-16 15:38:24 +0000

    add container check to integration test

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,7 +1,10 @@
 import os
+from constellation import docker_util
 from src.grout_deploy.cli import main
 
 def test_start_and_stop_grout():
     assert os.getenv("GITHUB_ACCESS_TOKEN") is not None, "GITHUB_ACCESS_TOKEN env var must be set to run integration test"
-    main(["start", "grout"])
+    main(["start", "grout", "--pull"])
+    assert docker_util.container_exists("grout")
     main(["stop"])
+    assert not docker_util.container_exists("grout")
